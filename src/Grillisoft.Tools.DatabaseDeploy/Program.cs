d4e3a4e47d10675980c707bee1ae09a841e7b278
Luigi Grilli <luigi.grilli@gmail.com>
2023-08-01 10:43:54 +0100

    Initial commit

---
+++ src/Grillisoft.Tools.DatabaseDeploy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grillisoft.Tools.DatabaseDeploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

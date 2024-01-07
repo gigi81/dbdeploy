433695dd45e1dfa50ded33823b73862841fb272e
Luigi Grilli <luigi.grilli@gmail.com>
2024-01-07 18:46:03 +0000

    Added more sqlserver implementation

--- src/Grillisoft.Tools.DatabaseDeploy.Abstractions/IDatabase.cs
+++ src/Grillisoft.Tools.DatabaseDeploy.Abstractions/IDatabase.cs
@@ -8,7 +8,7 @@ public interface IDatabase
     IScriptParser ScriptParser { get; }
     Task RunScript(string script, CancellationToken cancellationToken);
     Task InitializeMigrations(CancellationToken cancellationToken);
-    Task<DatabaseMigration[]> GetMigrations(CancellationToken cancellationToken);
+    Task<ICollection<DatabaseMigration>> GetMigrations(CancellationToken cancellationToken);
     Task AddMigration(DatabaseMigration migration, CancellationToken cancellationToken);
     Task RemoveMigration(DatabaseMigration migration, CancellationToken cancellationToken);
 }
--- src/Grillisoft.Tools.DatabaseDeploy.SqlServer/SqlServerDatabase.cs
+++ src/Grillisoft.Tools.DatabaseDeploy.SqlServer/SqlServerDatabase.cs
@@ -8,12 +8,21 @@ namespace Grillisoft.Tools.DatabaseDeploy.SqlServer;
 internal class SqlServerDatabase : IDatabase, IAsyncDisposable
 {
     private readonly SqlConnection _connection;
+    private readonly string _migrationTableName;
     private readonly SqlServerScriptParser _parser;
-
-    public SqlServerDatabase(string connectionString, SqlServerScriptParser parser)
+    private readonly string _getSql;
+    private readonly string _addSql;
+    private readonly string _deleteSql;
+    
+    public SqlServerDatabase(string connectionString, string migrationTableName, SqlServerScriptParser parser)
     {
         _connection = new SqlConnection(connectionString);
+        _migrationTableName = migrationTableName;
         _parser = parser;
+
+        _getSql = $"SELECT * FROM {migrationTableName}";
+        _addSql = $"INSERT INTO {migrationTableName} VALUES(@name)";
+        _deleteSql = $"DELETE FROM {migrationTableName} WHERE name = @name";
     }
 
     public IScriptParser ScriptParser => _parser;
@@ -21,18 +30,58 @@ internal class SqlServerDatabase : IDatabase, IAsyncDisposable
     public async Task RunScript(string script, CancellationToken cancellationToken)
     {
         await OpenConnection(cancellationToken);
-        await using var command = _connection.CreateCommand();
-        command.CommandText = script;
+        await using var command = CreateCommand(script);
+        await command.ExecuteNonQueryAsync(cancellationToken);
+    }
+    
+    public Task InitializeMigrations(CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public async Task<ICollection<DatabaseMigration>> GetMigrations(CancellationToken cancellationToken)
+    {
+        await OpenConnection(cancellationToken);
+        await using var command = CreateCommand(_getSql);
+        await using var reader = await command.ExecuteReaderAsync(cancellationToken);
+        var ret = new List<DatabaseMigration>();
+        
+        while (await reader.ReadAsync(cancellationToken))
+        {
+            ret.Add(new DatabaseMigration
+            (
+                reader.GetString(0),
+                reader.GetDateTimeOffset(1),
+                reader.GetString(2),
+                reader.GetString(3))
+            );
+        }
+
+        return ret;
+    }
+
+    public async Task AddMigration(DatabaseMigration migration, CancellationToken cancellationToken)
+    {
+        await OpenConnection(cancellationToken);
+        await using var command = CreateCommand(_addSql);
+        command.Parameters.AddWithValue("name", migration.Name);
+        command.Parameters.AddWithValue("datetime", migration.DateTime);
+        command.Parameters.AddWithValue("user", migration.User);
+        command.Parameters.AddWithValue("hash", migration.Hash);
         await command.ExecuteNonQueryAsync(cancellationToken);
     }
 
-    public Task InitializeMigrations(CancellationToken cancellationToken) => throw new NotImplementedException();
-
-    public Task<DatabaseMigration[]> GetMigrations(CancellationToken cancellationToken) => throw new NotImplementedException();
-
-    public Task AddMigration(DatabaseMigration migration, CancellationToken cancellationToken) => throw new NotImplementedException();
-
-    public Task RemoveMigration(DatabaseMigration migration, CancellationToken cancellationToken) => throw new NotImplementedException();
+    public async Task RemoveMigration(DatabaseMigration migration, CancellationToken cancellationToken)
+    {
+        await OpenConnection(cancellationToken);
+        await using var command = CreateCommand(_deleteSql);
+        command.Parameters.AddWithValue("name", migration.Name);
+        await command.ExecuteNonQueryAsync(cancellationToken);
+    }
+    
+    private SqlCommand CreateCommand(string script)
+    {
+        var command = _connection.CreateCommand();
+        command.CommandText = script;
+        return command;
+    }
 
     private async Task OpenConnection(CancellationToken cancellationToken)
     {

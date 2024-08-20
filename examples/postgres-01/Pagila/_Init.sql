15f90388cfa5e50cb60201233e757d4a599c3ceb
Luigi Grilli <luigi.grilli@gmail.com>
2024-08-20 22:01:13 +0100

    Added release 1.1 for postgres

--- examples/postgres-01/Pagila/_Init.sql
+++ examples/postgres-01/Pagila/_Init.sql
@@ -481,29 +481,6 @@ CREATE TABLE public.film_category (
 ALTER TABLE public.film_category OWNER TO postgres;
 
 --
--- Name: actor_info; Type: VIEW; Schema: public; Owner: postgres
---
-
-CREATE VIEW public.actor_info AS
- SELECT a.actor_id,
-    a.first_name,
-    a.last_name,
-    public.group_concat(DISTINCT ((c.name || ': '::text) || ( SELECT public.group_concat(f.title) AS group_concat
-           FROM ((public.film f
-             JOIN public.film_category fc_1 ON ((f.film_id = fc_1.film_id)))
-             JOIN public.film_actor fa_1 ON ((f.film_id = fa_1.film_id)))
-          WHERE ((fc_1.category_id = c.category_id) AND (fa_1.actor_id = a.actor_id))
-          GROUP BY fa_1.actor_id))) AS film_info
-   FROM (((public.actor a
-     LEFT JOIN public.film_actor fa ON ((a.actor_id = fa.actor_id)))
-     LEFT JOIN public.film_category fc ON ((fa.film_id = fc.film_id)))
-     LEFT JOIN public.category c ON ((fc.category_id = c.category_id)))
-  GROUP BY a.actor_id, a.first_name, a.last_name;
-
-
-ALTER TABLE public.actor_info OWNER TO postgres;
-
---
 -- Name: address_address_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -591,54 +568,6 @@ CREATE TABLE public.country (
 ALTER TABLE public.country OWNER TO postgres;
 
 --
--- Name: customer_list; Type: VIEW; Schema: public; Owner: postgres
---
-
-CREATE VIEW public.customer_list AS
- SELECT cu.customer_id AS id,
-    ((cu.first_name || ' '::text) || cu.last_name) AS name,
-    a.address,
-    a.postal_code AS "zip code",
-    a.phone,
-    city.city,
-    country.country,
-        CASE
-            WHEN cu.activebool THEN 'active'::text
-            ELSE ''::text
-        END AS notes,
-    cu.store_id AS sid
-   FROM (((public.customer cu
-     JOIN public.address a ON ((cu.address_id = a.address_id)))
-     JOIN public.city ON ((a.city_id = city.city_id)))
-     JOIN public.country ON ((city.country_id = country.country_id)));
-
-
-ALTER TABLE public.customer_list OWNER TO postgres;
-
---
--- Name: film_list; Type: VIEW; Schema: public; Owner: postgres
---
-
-CREATE VIEW public.film_list AS
- SELECT film.film_id AS fid,
-    film.title,
-    film.description,
-    category.name AS category,
-    film.rental_rate AS price,
-    film.length,
-    film.rating,
-    public.group_concat(((actor.first_name || ' '::text) || actor.last_name)) AS actors
-   FROM ((((public.category
-     LEFT JOIN public.film_category ON ((category.category_id = film_category.category_id)))
-     LEFT JOIN public.film ON ((film_category.film_id = film.film_id)))
-     JOIN public.film_actor ON ((film.film_id = film_actor.film_id)))
-     JOIN public.actor ON ((film_actor.actor_id = actor.actor_id)))
-  GROUP BY film.film_id, film.title, film.description, category.name, film.rental_rate, film.length, film.rating;
-
-
-ALTER TABLE public.film_list OWNER TO postgres;
-
---
 -- Name: inventory_inventory_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -692,29 +621,6 @@ CREATE TABLE public.language (
 
 
 ALTER TABLE public.language OWNER TO postgres;
-
---
--- Name: nicer_but_slower_film_list; Type: VIEW; Schema: public; Owner: postgres
---
-
-CREATE VIEW public.nicer_but_slower_film_list AS
- SELECT film.film_id AS fid,
-    film.title,
-    film.description,
-    category.name AS category,
-    film.rental_rate AS price,
-    film.length,
-    film.rating,
-    public.group_concat((((upper("substring"(actor.first_name, 1, 1)) || lower("substring"(actor.first_name, 2))) || upper("substring"(actor.last_name, 1, 1))) || lower("substring"(actor.last_name, 2)))) AS actors
-   FROM ((((public.category
-     LEFT JOIN public.film_category ON ((category.category_id = film_category.category_id)))
-     LEFT JOIN public.film ON ((film_category.film_id = film.film_id)))
-     JOIN public.film_actor ON ((film.film_id = film_actor.film_id)))
-     JOIN public.actor ON ((film_actor.actor_id = actor.actor_id)))
-  GROUP BY film.film_id, film.title, film.description, category.name, film.rental_rate, film.length, film.rating;
-
-
-ALTER TABLE public.nicer_but_slower_film_list OWNER TO postgres;
 
 --
 -- Name: payment_payment_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
@@ -912,25 +818,6 @@ CREATE MATERIALIZED VIEW public.rental_by_category AS
 ALTER TABLE public.rental_by_category OWNER TO postgres;
 
 --
--- Name: sales_by_film_category; Type: VIEW; Schema: public; Owner: postgres
---
-
-CREATE VIEW public.sales_by_film_category AS
- SELECT c.name AS category,
-    sum(p.amount) AS total_sales
-   FROM (((((public.payment p
-     JOIN public.rental r ON ((p.rental_id = r.rental_id)))
-     JOIN public.inventory i ON ((r.inventory_id = i.inventory_id)))
-     JOIN public.film f ON ((i.film_id = f.film_id)))
-     JOIN public.film_category fc ON ((f.film_id = fc.film_id)))
-     JOIN public.category c ON ((fc.category_id = c.category_id)))
-  GROUP BY c.name
-  ORDER BY (sum(p.amount)) DESC;
-
-
-ALTER TABLE public.sales_by_film_category OWNER TO postgres;
-
---
 -- Name: staff_staff_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -992,49 +879,6 @@ CREATE TABLE public.store (
 
 
 ALTER TABLE public.store OWNER TO postgres;
-
---
--- Name: sales_by_store; Type: VIEW; Schema: public; Owner: postgres
---
-
-CREATE VIEW public.sales_by_store AS
- SELECT ((c.city || ','::text) || cy.country) AS store,
-    ((m.first_name || ' '::text) || m.last_name) AS manager,
-    sum(p.amount) AS total_sales
-   FROM (((((((public.payment p
-     JOIN public.rental r ON ((p.rental_id = r.rental_id)))
-     JOIN public.inventory i ON ((r.inventory_id = i.inventory_id)))
-     JOIN public.store s ON ((i.store_id = s.store_id)))
-     JOIN public.address a ON ((s.address_id = a.address_id)))
-     JOIN public.city c ON ((a.city_id = c.city_id)))
-     JOIN public.country cy ON ((c.country_id = cy.country_id)))
-     JOIN public.staff m ON ((s.manager_staff_id = m.staff_id)))
-  GROUP BY cy.country, c.city, s.store_id, m.first_name, m.last_name
-  ORDER BY cy.country, c.city;
-
-
-ALTER TABLE public.sales_by_store OWNER TO postgres;
-
---
--- Name: staff_list; Type: VIEW; Schema: public; Owner: postgres
---
-
-CREATE VIEW public.staff_list AS
- SELECT s.staff_id AS id,
-    ((s.first_name || ' '::text) || s.last_name) AS name,
-    a.address,
-    a.postal_code AS "zip code",
-    a.phone,
-    city.city,
-    country.country,
-    s.store_id AS sid
-   FROM (((public.staff s
-     JOIN public.address a ON ((s.address_id = a.address_id)))
-     JOIN public.city ON ((a.city_id = city.city_id)))
-     JOIN public.country ON ((city.country_id = country.country_id)));
-
-
-ALTER TABLE public.staff_list OWNER TO postgres;
 
 --
 -- Name: payment_p2022_01; Type: TABLE ATTACH; Schema: public; Owner: postgres

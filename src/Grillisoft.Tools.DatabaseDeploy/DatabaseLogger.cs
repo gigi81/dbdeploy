2ba00f68ddb1f3ebc3a2ccd07fa81d3e12efb630
Luigi Grilli <luigi.grilli@gmail.com>
2024-08-24 15:30:29 +0100

    Fixed database logger

--- src/Grillisoft.Tools.DatabaseDeploy/DatabaseLogger.cs
+++ src/Grillisoft.Tools.DatabaseDeploy/DatabaseLogger.cs
@@ -4,26 +4,51 @@ namespace Grillisoft.Tools.DatabaseDeploy;
 
 public class DatabaseLogger : ILogger
 {
-    private readonly string _database;
+    private static Type? FormattedLogValuesType = Type.GetType("Microsoft.Extensions.Logging.FormattedLogValues, Microsoft.Extensions.Logging.Abstractions");
+
+    private readonly string[] _database;
     private readonly ILogger _logger;
+    private readonly string _prefixFormat;
 
     public DatabaseLogger(string database, ILogger logger)
     {
-        _database = database;
+        _database = [database];
         _logger = logger;
+        _prefixFormat = "Database {Database}: ";
     }
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
         Func<TState, Exception?, string> formatter)
     {
-        _logger.Log(logLevel, eventId, state, exception, GetFormatter(formatter));
+        if (!IsEnabled(logLevel))
+            return;
+        
+        state = CreateNewState(state);
+        _logger.Log(logLevel, eventId, state, exception, formatter);
     }
 
-    private Func<TState,Exception?,string> GetFormatter<TState>(Func<TState,Exception?,string> formatter)
+    /// <summary>
+    /// See reason why we have to do this horror here:
+    /// https://github.com/dotnet/runtime/issues/67577#issuecomment-2291356471
+    /// </summary>
+    /// <param name="state"></param>
+    /// <typeparam name="TState"></typeparam>
+    /// <returns></returns>
+    private TState CreateNewState<TState>(TState state)
     {
-        return (state, exception) => $"Database {_database}: " + formatter(state, exception);
-    }
+        if (FormattedLogValuesType == null || typeof(TState) != FormattedLogValuesType)
+            return state;
 
+        if (state is not IReadOnlyList<KeyValuePair<string, object>> keyValuePairs)
+            return state;
+
+        //the last item of keyValuePairs contains the original format
+        var values = _database.Concat(keyValuePairs.Take(keyValuePairs.Count - 1).Select(kvp => kvp.Value)).ToArray();
+        var format = _prefixFormat + keyValuePairs[^1].Value;
+        
+        return (TState?)Activator.CreateInstance(FormattedLogValuesType, format, values) ?? state;
+    }
+    
     public bool IsEnabled(LogLevel logLevel)
     {
         return _logger.IsEnabled(logLevel);

131d93658e84d2c08a927629d75bca4584cffea4
Luigi Grilli <luigi.grilli@gmail.com>
2025-07-07 21:31:27 +0100

    Upgraded all to .NET 9

--- src/Grillisoft.Tools.DatabaseDeploy.AI/Extensions.cs
+++ src/Grillisoft.Tools.DatabaseDeploy.AI/Extensions.cs
@@ -2,6 +2,7 @@ using Grillisoft.Tools.DatabaseDeploy.Abstractions;
 using Grillisoft.Tools.DatabaseDeploy.AI;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
+using OllamaSharp;
 
 // ReSharper disable once CheckNamespace
 namespace Grillisoft.Tools.DatabaseDeploy;
@@ -10,7 +11,7 @@ public static class Extensions
 {
     public static IServiceCollection AddAIGenerator(this IServiceCollection services)
     {
-        services.AddSingleton<IChatClient>((_) => new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.1"));
+        services.AddSingleton<IChatClient>((_) => new OllamaApiClient(new Uri("http://localhost:11434/"), "llama3.1"));
         services.AddSingleton<IGenerator, Generator>();
         return services;
     }

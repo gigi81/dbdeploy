f383e865dfb57af92c896905a46ca6ddff5a8c95
Luigi Grilli <luigi.grilli@gmail.com>
2026-07-29 20:21:11 +0100

    Code fix

--- tests/Grillisoft.Tools.DatabaseDeploy.MySql.Tests/MySqlDatabaseTests.cs
+++ tests/Grillisoft.Tools.DatabaseDeploy.MySql.Tests/MySqlDatabaseTests.cs
@@ -1,4 +1,5 @@
 ﻿using Grillisoft.Tools.DatabaseDeploy.Abstractions;
+using Grillisoft.Tools.DatabaseDeploy.Tests;
 using Grillisoft.Tools.DatabaseDeploy.Tests.Databases;
 using Testcontainers.MySql;
 using Xunit.Abstractions;
